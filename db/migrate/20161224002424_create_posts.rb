0457a6f6213fb379212dcdfe120e641676274203
Mike Barile <mikebarile13@gmail.com>
2016-12-23 16:31:19 -0800

    Created postgres tables for users, posts, and comments

---
+++ db/migrate/20161224002424_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :user_id, null: false
+      t.string :image_url, null: false
+
+      t.timestamps
+    end
+  end
+end
