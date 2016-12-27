48aef59d6d7c6c341775eea5ab969396d1573f5d
Mike Barile <mikebarile13@gmail.com>
2016-12-27 11:11:01 -0800

    Copied in header and footer components

---
+++ frontend/components/root.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-router';
+import App from './app.jsx';
+import HomeContainer from './home/home_container';
+
+
+const Root = ({ store }) => {
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  };
+
+  return (
+  <Provider store={store}>
+    <Router history={hashHistory} onUpdate={() => window.scrollTo(0, 0)}>
+      <Route path="/" component={App} >
+      </Route>
+    </Router>
+  </Provider>
+);
+};
+
+export default Root;
