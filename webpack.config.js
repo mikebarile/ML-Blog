26188d53302c1238d5cf8bc0294a10b9a8db805a
Mike Barile <mikebarile13@gmail.com>
2016-12-27 10:24:50 -0800

    Added npm packge bundle, front end entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
   context: __dirname,
-  entry: './frontend/hair_bnb.jsx',
+  entry: './frontend/ml_blogger.jsx',
   output: {
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
